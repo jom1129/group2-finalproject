9d1e07196c749bcb18d1be567cde675b004d3b2b
adi <adi@fpcph-mbp.local>
2021-05-03 11:45:54 +0800

    added getter and setter methods for the Citizen class

--- src/prog2/finalgroup/Citizen.java
+++ src/prog2/finalgroup/Citizen.java
@@ -14,6 +14,62 @@ public class Citizen implements Comparable{
     private int district;
     private char gender; // should store M for males and F for females
 
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isResident() {
+        return resident;
+    }
+
+    public int getDistrict() {
+        return getDistrict();
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setResident(boolean resident) {
+        this.resident = resident;
+    }
+
+    public void setDistrict(int district) {
+        this.district = district;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
     @Override
     public int compareTo(Object o) {
         return 0;
